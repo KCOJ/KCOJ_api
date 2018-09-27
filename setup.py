2b0a1a33daa893c3d56191b64f1a2b0f07b9df21
Pin Lin <moneycat711@gmail.com>
2018-09-27 11:04:45 +0800

    Show README.md on PyPI

--- setup.py
+++ setup.py
@@ -1,9 +1,17 @@
 from setuptools import setup, find_packages
+from os import path
+
+# read the contents of your README file
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
 
 setup(
     name='KCOJ_api',
-    version='1.1',
+    version='1.5',
     description='A Python Module for get data from real KCOJ.',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     author='pinlin',
     author_email='moneycat711@gmail.com',
     license='MIT License',
